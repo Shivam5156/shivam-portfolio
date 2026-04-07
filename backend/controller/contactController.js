e50ca8a002e7f4df6fa790f01b01c515b365484c
shivam singh <singhs36533@gmail.com>
2026-04-08 04:00:34 +0530

    debuginh

--- backend/controller/contactController.js
+++ backend/controller/contactController.js
@@ -14,6 +14,8 @@ const sendContactMail = async (req, res) => {
             })
         }
 
+        console.log(process.env.USER_MAIL);
+        
         const mailOptions = {
             from: process.env.USER_MAIL,
             to: process.env.USER_MAIL,
