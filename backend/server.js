12920be52eb4670fc3f33a53f38b666fa3477ef6
shivam singh <singhs36533@gmail.com>
2026-07-31 21:13:17 +0530

    bug fix

--- backend/server.js
+++ backend/server.js
@@ -11,7 +11,7 @@ const PORT = process.env.PORT || 5000;
 const allowedOrigins = [
   'http://localhost:5173',
   'https://shivam-portfolio-git-main-shivam5156s-projects.vercel.app',
-  "https://shivam-portfolio-wheat-kappa.vercel.app"
+  'https://shivam-portfolio-wheat-kappa.vercel.app'
 ];
 
 app.use(cors({
